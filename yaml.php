36c1caa386626d8b24fb35c8922615dcfe8d356e
Essuie-glace <axel.hervieu@sts-sio-caen.info>
2021-11-18 11:58:04 +0100

    add yaml parsing

--- yaml.php
+++ yaml.php
@@ -1,19 +1,24 @@
+<?php
+	$content=file_get_contents('test.yaml');
+	$array=yaml_parse($content);
+	$menu=$array['menu'];
+
+?>
 <!DOCTYPE html>
 <html>
 	<head>
-		<title>Axel site</title>
+		<meta charset="utf-8"/>
+		<title>Site Axel</title>
 		<link rel="stylesheet" href="">
 	</head>
 <body>
+<h1><?=$array['moi']['prenom']?></h1>
 <?php
-	$content=file_get_contents('test.yaml');
-	$menu=yaml_parse($content)['menu'];
 	echo "<ul>";
 	foreach($menu as $item){
 		echo "<li>$item</li>";
 	}
-	echo "</ul>";
-
+	echo "<ul>";
 ?>
 </body>
 </html>
